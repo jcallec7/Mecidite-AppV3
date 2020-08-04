c80f5791b90daabf088ac50ce5673e10d65c3aa3
jcallec7 <jcallec7@hotmail.com>
2020-08-04 14:11:20 -0500

    Firebase agregado

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,17 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebaseConfig: {
+    apiKey: "AIzaSyCqVyOALcBuAKp04ifb0a5TDzWIi17tM6c",
+    authDomain: "mediciteapp.firebaseapp.com",
+    databaseURL: "https://mediciteapp.firebaseio.com",
+    projectId: "mediciteapp",
+    storageBucket: "mediciteapp.appspot.com",
+    messagingSenderId: "300913221142",
+    appId: "1:300913221142:web:91649e0c2545a648b69f37",
+    measurementId: "G-6W79L8TZCZ"
+  }
 };
 
 /*
