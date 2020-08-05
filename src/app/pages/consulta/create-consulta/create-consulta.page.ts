ff69b0b861ac737d091655d67c360009d1af20f2
PedrojOrtiz <pedroj_ortiz@outlook.com>
2020-08-05 13:46:27 -0500

    pasando consulta del repo anterior

--- src/app/pages/consulta/create-consulta/create-consulta.page.ts
+++ src/app/pages/consulta/create-consulta/create-consulta.page.ts
@@ -1,4 +1,10 @@
 import { Component, OnInit } from '@angular/core';
+import { Consulta } from 'src/app/model/Consulta';
+import { ActivatedRoute, Router, NavigationExtras } from '@angular/router';
+import { Observable } from 'rxjs';
+import { Usuario } from 'src/app/model/Usuario';
+import { ConsultaService } from 'src/app/services/consulta-service/consulta.service';
+import { AuthenticationService } from 'src/app/services/login/authentication.service';
 
 @Component({
   selector: 'app-create-consulta',
@@ -7,9 +13,91 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CreateConsultaPage implements OnInit {
 
-  constructor() { }
+  consulta: Consulta
+
+  fechamin: string;
+  fechamax: string;
+  horas: string[];
+
+  paciente: Usuario;
+
+  medicos: Observable<Usuario[]>;
+  medicoSelected: Usuario;
+
+  constructor(private consultaService: ConsultaService, 
+              private route: ActivatedRoute, 
+              public router: Router, 
+              public auth: AuthenticationService) { }
 
   ngOnInit() {
+
+    this.medicos = this.consultaService.getMedicos();
+    this.auth.getCurrentUser().then(user=>{
+      console.log(user)
+    });
+
+    this.consulta = {
+
+      uid: "",
+      paciente: null,
+      medico: null,
+      estado: "",
+      fecha: new Date().toISOString(),
+      diagnostico: null
+  
+    }
+
+		console.log("consulta inicializada: " + JSON.stringify(this.consulta))
+    
+    const d = new Date();
+    d.setDate(d.getDate() + 60);
+    this.fechamin = d.toISOString();
+    this.fechamax = d.toISOString(); 
+    
+    /*
+    this.consulta.fecha = ;
+    this.consulta.fecha = moment(this.consulta.fecha).toISOString();
+    this.fechamin = moment().subtract(5, 'h').format();
+    this.fechamin = moment(this.fechamin).toISOString();*/
+
+    //console.log(this.fechamin + "fecha consulta: " + this.consulta.fecha);
+    
+    //this.fechamax = moment().add(60, 'd').format();
+    //this.fechamax = moment(this.fechamax).toISOString();
+
+    //console.log(this.fechamin + "fecha consulta: " + this.consulta.fecha);
+
+
+  }
+
+  onChange() {
+    console.log("Selected: " + this.medicoSelected + " uid: " + this.medicoSelected.uid);
+  }
+
+  async createConsulta() {
+
+    this.consulta.medico = this.medicoSelected;
+
+    // cargar datos de la sesion:
+    console.log("obteniendo datos paciente ");
+    this.consulta.paciente = await this.consultaService.getUsuarioById('Blvbt2LTeCNkv3NtqfAaOVRw4ff1');
+    console.log("Paciente: " + this.consulta.paciente.nombre);
+    console.log("Medico: " + this.medicoSelected.uid);
+
+    //
+    this.consulta.estado = "Creada, pendiente de pago";
+
+    console.log("sending consult: " + JSON.stringify(this.consulta) ) ;
+
+    /*let navigationExtras: NavigationExtras = {
+      queryParams: {
+          'consulta': this.consulta
+      }
+    };*/
+    //this.consultaService.createConsulta(this.consulta, this.medicoSelected.uid, this.consulta.paciente.uid);
+
+    this.router.navigate([`factura/${this.consulta}`]);
+
   }
 
 }
--- src/app/services/consulta-service/consulta.service.ts
+++ src/app/services/consulta-service/consulta.service.ts
@@ -1,9 +1,59 @@
 import { Injectable } from '@angular/core';
+import { AngularFirestore } from '@angular/fire/firestore';
+import { Consulta } from 'src/app/model/Consulta';
+import { Observable } from 'rxjs';
+import { Usuario } from 'src/app/model/Usuario';
+import { first } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ConsultaService {
 
-  constructor() { }
+  constructor(private afs: AngularFirestore) { }
+
+  getMedicos(): Observable<any[]> {
+    return this.afs.collection('usuarios',
+      ref => ref.where('rol', '==', 
+      this.afs.collection('roles').doc('medico').ref))
+      .valueChanges();
+  }
+
+  async getUsuarioById(uid: string): Promise<Usuario> {
+    try{
+        let aux:any = await this.afs.collection("usuarios", 
+            ref => ref.where('uid', '==', uid))
+                      .valueChanges().pipe(first()).toPromise().then(doc => {                    	  
+                          return doc;
+                      }).catch(error => {
+                          throw error;
+                      });
+        if(aux.length==0)
+            return undefined;
+        return aux[0];
+    }catch(error){
+      console.error("Error", error);
+      throw error;
+    } 
+  }
+
+  createConsulta(consulta: Consulta, medicoUid: string, pacienteUid: string) {
+    
+    let auxConsulta = consulta;
+
+    const refConsulta = this.afs.collection('consultas');
+    
+    auxConsulta.paciente = null;
+    auxConsulta.medico = null;
+    auxConsulta.uid = this.afs.createId();
+    const param = JSON.parse(JSON.stringify(auxConsulta));
+    
+    refConsulta.doc(auxConsulta.uid).set(param);
+
+    this.afs.collection("consultas").doc(auxConsulta.uid).update({
+      paciente: this.afs.collection("usuarios").doc(pacienteUid).ref,
+      medico: this.afs.collection("usuarios").doc(medicoUid).ref});
+
+  }
+
 }
